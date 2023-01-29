2edf9fe71c7a25397f713d72bd58a3db22c47af4
Lisa <elizabethpham726@gmail.com>
2023-01-29 14:51:08 -0800

    Passes down categoryKey prop to SuggestionScreen

--- screens/SuggestionScreen.js
+++ screens/SuggestionScreen.js
@@ -1,17 +1,21 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { globalStyles } from '../styles/global';
+import suggestionsData from '../assets/data/idea-data.json';
+import SuggestionCard from '../components/SuggestionCard';
 
 export default function SuggestionScreen({ route, navigation }) {
   // getting the params
-
   const { categoryKey } = route.params;
+
+  const getCategorySuggestions = (categoryKey) => {};
 
   return (
     <SafeAreaView style={globalStyles.container}>
       <Text>{categoryKey}</Text>
-      <Text>Suggestion #2</Text>
+      {/* <Text>{suggestionsData}</Text> */}
       <Text>Suggestion #3</Text>
+      <SuggestionCard suggestion={'this is a test'} />
     </SafeAreaView>
   );
 }
