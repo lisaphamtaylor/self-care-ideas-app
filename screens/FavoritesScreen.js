a8c69b5a0b936b8fab496ee1410be4508e940b0c
Lisa <113562541+lisaphamtaylor@users.noreply.github.com>
2023-02-12 22:19:17 -0800

    sets up FavoritesScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,8 +1,21 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { globalStyles } from '../styles/Global';
+import { doc, getDoc } from 'firebase/firestore';
+import { currentDate, uid } from './FilterScreen';
+import { db } from '../firebase';
 
 const FavoritesScreen = () => {
+  if (uid !== null) {
+    const docRef = doc(db, 'users', uid, 'date', currentDate);
+    const docSnap = getDoc(docRef);
+
+    if (docSnap) {
+      console.log('Document data:', docSnap.data);
+    }
+  } else {
+  }
+
   return (
     <SafeAreaView style={globalStyles.container}>
       <View>
